631fa952e58e1ec4fdaed40d71f355cb9759fbdd
Youcef <youcefcopy@gmail.com>
2026-02-20 11:20:48 +0100

    the issue was the name of the file, Duh!

--- src/sound/click.js
+++ src/sound/click.js
@@ -1,5 +1,5 @@
 // here are the click object
-const clickSound1 = new Audio('/sound/click.mp3')
+const clickSound1 = new Audio('/sound/click-sound2.mp3')
 
 const playClickSound = () => {
   clickSound1.play().catch((err) => {
