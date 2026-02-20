7347a9cbc2181dead00f5356bbd7e700c0ea026e
Youcef <youcefcopy@gmail.com>
2026-02-20 14:30:37 +0100

    icons section has been styled

--- src/components/ManuscriptIndex.vue
+++ src/components/ManuscriptIndex.vue
@@ -3,10 +3,10 @@
 
 <template>
   <div class="kanji">
-    <div class="icon">我 <span>About</span></div>
-    <div class="icon">匠 <span>Work</span></div>
-    <div class="icon">縁 <span>Links</span></div>
-    <div class="icon">交 <span>Conctact Me</span></div>
+    <div class="icon-field">我 <span>About</span></div>
+    <div class="icon-field">匠 <span>Work</span></div>
+    <div class="icon-field">縁 <span>Links</span></div>
+    <div class="icon-field">交 <span>Conctact</span></div>
     <!-- 我 (one self) 匠 (Craft/Master)
     <br />
     縁 (connections)
@@ -23,15 +23,26 @@
   border: 3px solid red;
   height: 500px;
   margin: 0.5rem;
+  display: flex;
 }
 
-.icon {
+.icon-field {
   font-size: 4rem;
   font-family: 'Yuji Boku', sans-serif;
   font-weight: 600;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  cursor: pointer;
+
+  background-color: seagreen;
 }
 
 span {
   font-family: 'Yuji Boku', sans-serif;
+  /* display: block; */
+  font-size: 1.3rem;
 }
 </style>
